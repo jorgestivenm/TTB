2504496faa7e8460a3b8559e84f6fc292fe2185c
jorgestivenm1 <steven.martinez@udea.edu.co>
2021-07-20 14:00:21 -0500

    adding .env file

--- src/data/db/dynamoDBConfig.js
+++ src/data/db/dynamoDBConfig.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const awsConfig = {
   region: "us-east-2",
   endpoint: "http://dynamodb.us-east-2.amazonaws.com",
-
+  accessKeyId: process.env.ACCESS_KEY_ID_AWS,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY_AWS
 };
 
 module.exports = awsConfig;
--- src/data/db/dynamoDBConnection.js
+++ src/data/db/dynamoDBConnection.js
@@ -2,7 +2,6 @@ var AWS = require('aws-sdk');
 const awsconfig = require('./dynamoDBConfig');
 
 AWS.config.update(awsconfig);
-
 var clientDB = new AWS.DynamoDB();
 
 module.exports = clientDB;
