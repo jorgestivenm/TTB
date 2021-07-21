3e97d4639ceefbe6b8d2d5884864697bd005026b
jorgestivenm1 <steven.martinez@udea.edu.co>
2021-07-20 21:34:35 -0500

    add a github action to create dotenv file

--- src/data/db/dynamoDBConfig.js
+++ src/data/db/dynamoDBConfig.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 const awsConfig = {
-  region: "us-east-2",
-  endpoint: "http://dynamodb.us-east-2.amazonaws.com",
+  region: process.env.REGION,
+  endpoint: process.env.ENDPOINT,
   accessKeyId: process.env.ACCESS_KEY_ID_AWS,
   secretAccessKey: process.env.SECRET_ACCESS_KEY_AWS
 };
